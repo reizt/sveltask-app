533026fa8c28df23b94555b7776d3958bf2709cd
reizt <reizt.dev@gmail.com>
2023-06-27 18:07:52 +0900

    PrettierとESLint

--- src/routes/sverdle/reduced-motion.ts
+++ src/routes/sverdle/reduced-motion.ts
@@ -1,23 +1,24 @@
-import { readable } from 'svelte/store';
 import { browser } from '$app/environment';
+import { readable } from 'svelte/store';
 
-const reduced_motion_query = '(prefers-reduced-motion: reduce)';
+const reducedMotionQuery = '(prefers-reduced-motion: reduce)';
 
-const get_initial_motion_preference = () => {
-	if (!browser) return false;
-	return window.matchMedia(reduced_motion_query).matches;
+const getInitialMotionPreference = () => {
+  if (!browser) return false;
+  return window.matchMedia(reducedMotionQuery).matches;
 };
 
-export const reduced_motion = readable(get_initial_motion_preference(), (set) => {
-	if (browser) {
-		const set_reduced_motion = (event: MediaQueryListEvent) => {
-			set(event.matches);
-		};
-		const media_query_list = window.matchMedia(reduced_motion_query);
-		media_query_list.addEventListener('change', set_reduced_motion);
+export const reducedMotion = readable(getInitialMotionPreference(), (set) => {
+  if (browser) {
+    const setReducedMotion = (event: MediaQueryListEvent) => {
+      set(event.matches);
+    };
+    const mediaQueryList = window.matchMedia(reducedMotionQuery);
+    mediaQueryList.addEventListener('change', setReducedMotion);
 
-		return () => {
-			media_query_list.removeEventListener('change', set_reduced_motion);
-		};
-	}
+    return () => {
+      mediaQueryList.removeEventListener('change', setReducedMotion);
+    };
+  }
+  return () => {};
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
+/// <reference types="vitest" />
+
 import { sveltekit } from '@sveltejs/kit/vite';
+import { resolve } from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [sveltekit()],
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}'],
+    globals: true,
+  },
+  resolve: {
+    alias: {
+      '#': resolve(__dirname, './src'),
+    },
+  },
 });
